9436046f898909c351cbdb19b15a6f95a807f039
1Jayso <adodey1@yahoo.com>
2022-12-08 22:48:26 +0000

    modified the code to match  the new changes on git.ir

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # git_ir-downloader
+
+This is a simple python application that allows users of git.ir website to download video tutorials.
+
+To use the application:
+1. Install python python3
+2. Create a virtual environmanet 
+3. Run pip install -r requirements.txt
+4. Start the application by typing python gitir_downloader.py 
+5. Enter the url after clciking view links from git.ir tutorial page.
+
+To contribute to making this app better for use kindly read the contribution.md file.
+
+Thanks :)
--- gitir_downloader.py
+++ gitir_downloader.py
@@ -2,11 +2,21 @@ from genericpath import isfile
 from bs4 import BeautifulSoup
 import wget
 import os
+from pathlib import Path
+import urllib.request, urllib.error, urllib.parse
+import linecache
+import shutil
 
 text_url = input("Paste url to download the text file: ")
 txt = text_url.split('/')[-2]
 #the index of the slit could change
 dir_name = txt.split('--')[-1] 
+file_name = 'saved_file.txt'
+
+absolute_path = os.path.dirname(__file__)
+relative_path = dir_name
+full_path = os.path.join(absolute_path, relative_path)
+
 
 
 def create_dir(path):
@@ -22,14 +32,17 @@ def create_dir(path):
     except OSError:
         print("Creation of the directory %s failed" % path)
 
-
 create_dir(path=dir_name)
 
-textfile = text_url.split('/')[2]
 
-
+folder_name = []
 def open_file():
-    with open(textfile, 'r+') as fp:
+    line = linecache.getline(file_name, 2)
+    new_folder_name = line.split('/')[-2] 
+    folder_name.append(new_folder_name)
+    print(line)
+    with open(file_name, 'r+') as fp:
+        
         # read an store all lines into list
         lines = fp.readlines()
         # move file pointer to the beginning of a file
@@ -40,45 +53,37 @@ def open_file():
         # start writing lines
         # iterate line and line number
         for number, line in enumerate(lines):
-            
             # note: list index start from 0
             if number not in [0, 1, 2]:
                 fp.write(line)
 
 
-def download_text_file(text_url):
+
+def save_file(file_name, text_url):
     try:
-        isFile = os.path.isfile(textfile)
-        if isFile:
-            print()
-            print("File already exist !!")
-            open_file()
-        else:
-            wget.download(text_url)
-            print("\nText file has been downloaded!!")
-            open_file()
-            print()
+        response = urllib.request.urlopen(text_url)
+        webContent = response.read().decode('UTF-8')
+        # print(webContent)
+        f = open(file_name, 'w')
+        f.write(webContent)
+        f.close
+        print("\nText file has been downloaded!!")
+        print()
     except OSError:
         print(f"Downloding of {text_url} failed")
-
-
-download_text_file(text_url)
+save_file(file_name, text_url)
+open_file()
 
 
 links = []
-
-
-def list_of_downloads(): 
+def download_links(): 
     try:
-        lines = [line.strip() for line in open(textfile, 'r+')]
+        lines = [line.strip() for line in open(file_name, 'r+')]
         links.append(lines)
-
         print(sorted(links))
     except OSError:
         print(OSError)
-
-
-list_of_downloads()
+download_links()
 
 
 def download_videos():
@@ -88,11 +93,11 @@ def download_videos():
         print("Downloding Videos ...")
         for dl in link:
             file_name = dl.split('/')[-1] 
+            file_name = file_name.replace("-%20", " ")
             print(file_name)
 
             if os.path.exists(file_name):
                 print(file_name, "already downloaded")
-
             else:
                 print(f"Downloading file {file_name}")
                 # start Download
@@ -102,10 +107,22 @@ def download_videos():
                 print()
         return
 
-
-
+def cleanupTempFiles(folder_path):
+    full_path = os.path.join(absolute_path, folder_path)
+    os.chdir(full_path)
+    folder = os.listdir(full_path)
+    for item in folder:
+        if item.endswith(".tmp"):
+            os.remove(os.path.join(full_path, item))
+        else:
+            print("There are no .tmp files in this directory")
+    print("All .tmp Files have been deleted successfully")
+    
 
 download_videos()
+os.chdir(absolute_path)
+shutil.move(full_path, folder_name[0])
+cleanupTempFiles(folder_name[0])
 
 
 # https://git.ir/media/download-links/packt-data-structures-and-algorithms-the-complete--b216f699b98f467395115dbc6691e429.txt
